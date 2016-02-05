1ee36dabd6b0b5063348d5994b859a9fc08b4875
Ara Vardanyan <aravardanyan@live.com>
2016-02-06 00:00:49 +0400

    some improvements

--- app/components/app.ts
+++ app/components/app.ts
@@ -7,4 +7,33 @@ import {View} from "angular2/core";
 @View({
     templateUrl: '../../app/templates/layout.html'
 })
-export class AppComponent { }
+export class AppComponent {
+
+    constructor () {
+        this._firstName = 'Ara';
+        this._lastName = 'Vardanyan';
+        this._age = 34;
+        this._email = 'aravardanyan@gmail.com';
+    }
+
+    private _firstName: string;
+    private _lastName: string;
+    private _age: number;
+    private _email: string;
+
+    get firstName(): string {
+        return this._firstName;
+    }
+
+    get lastName(): string {
+        return this._lastName;
+    }
+
+    get age(): number {
+        return this._age;
+    }
+
+    get email(): string {
+        return this._email;
+    }
+}
