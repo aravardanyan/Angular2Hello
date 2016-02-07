3be6e03442a5a4c36d4865d9d2a3a021dc26b63b
Ara Vardanyan <aravardanyan@live.com>
2016-02-07 12:00:19 +0400

    refactoring

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,5 @@
 import {bootstrap}    from 'angular2/platform/browser'
-import {AppComponent} from './components/app'
+import {AppComponent} from './components/app.component'
 import {UserService} from "./services/user.service";
 
 bootstrap(AppComponent);
